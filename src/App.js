a0f7483ca78c9ca4726769227f2b72ca9bdcd132
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-23 19:30:32 -0700

    implement GraphQL Pagination

--- src/App.js
+++ src/App.js
@@ -6,57 +6,77 @@ const TITLE = 'React GraphQL GitHub Client';
 const axiosGitHubGraphQL = axios.create({
   baseURL: 'https://api.github.com/graphql',
   headers: {
-    Authorization: `bearer ca43968afc198e8146242db6f333fb2b46b12676`,
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+      }`,
   },
 });
 
-const GET_ORGANIZATION = `
-  {
-    organization(login: "the-road-to-learn-react") {
+const GET_ISSUES_OF_REPOSITORY = `
+  query ($organization: String!, $repository: String!) {
+    organization(login: $organization) {
       name
       url
+      repository(name: $repository) {
+        name
+        url
+        issues(last: 5) {
+          edges {
+            node {
+              id
+              title
+              url
+            }
+          }
+        }
+      }
     }
   }
 `;
 
+const getIssuesOfRepository = path => {
+  const [organization, repository] = path.split('/');
 
+  return axiosGitHubGraphQL.post('', {
+    query: GET_ISSUES_OF_REPOSITORY,
+    variables: { organization, repository },
+  });
+};
+
+const resolveIssuesQuery = queryResult => () => ({
+  organization: queryResult.data.data.organization,
+  errors: queryResult.data.errors,
+});
 
 class App extends Component {
   state = {
     path: 'the-road-to-learn-react/the-road-to-learn-react',
     organization: null,
-    errors: null
+    errors: null,
   };
 
   componentDidMount() {
-    this.onFetchFromGitHub();
+    this.onFetchFromGitHub(this.state.path);
   }
 
-  onChange = e => {
-    this.setState({ path: e.target.value });
-  }
-
-  onSubmit = e => {
-    e.preventDefault();
+  onChange = event => {
+    this.setState({ path: event.target.value });
   };
 
-  onFetchFromGitHub = async () => {
-    try {
+  onSubmit = event => {
+    this.onFetchFromGitHub(this.state.path);
 
-      const result = await axiosGitHubGraphQL
-        .post('', { query: GET_ORGANIZATION });
-      this.setState(() => ({
-        organization: result.data.data.organization,
-        errors: result.data.errors
-      }));
-      console.log('organization', this.organization)
-    } catch (e) {
-      console.log(e);
-    }
+    event.preventDefault();
+  };
+
+  onFetchFromGitHub = path => {
+    getIssuesOfRepository(path).then(queryResult =>
+      this.setState(resolveIssuesQuery(queryResult)),
+    );
   };
 
   render() {
-    const { path, organization } = this.state;
+    const { path, organization, errors } = this.state;
 
     return (
       <div>
@@ -65,10 +85,11 @@ class App extends Component {
         <form onSubmit={this.onSubmit}>
           <label htmlFor="url">
             Show open issues for https://github.com/
-        </label>
+          </label>
           <input
             id="url"
             type="text"
+            value={path}
             onChange={this.onChange}
             style={{ width: '300px' }}
           />
@@ -76,24 +97,53 @@ class App extends Component {
         </form>
 
         <hr />
+
         {organization ? (
-          <Organization organization={organization} />
+          <Organization organization={organization} errors={errors} />
         ) : (
             <p>No information yet ...</p>
-          )
-        }
+          )}
       </div>
     );
   }
 }
 
-const Organization = ({ organization }) => (
-  < div >
+const Organization = ({ organization, errors }) => {
+  if (errors) {
+    return (
+      <p>
+        <strong>Something went wrong:</strong>
+        {errors.map(error => error.message).join(' ')}
+      </p>
+    );
+  }
+
+  return (
+    <div>
+      <p>
+        <strong>Issues from Organization:</strong>
+        <a href={organization.url}>{organization.name}</a>
+      </p>
+      <Repository repository={organization.repository} />
+    </div>
+  );
+};
+
+const Repository = ({ repository }) => (
+  <div>
     <p>
-      <strong>Issues from Organization: </strong>
-      <a href={organization.url}>{organization.name}</a>
+      <strong>In Repository:</strong>
+      <a href={repository.url}>{repository.name}</a>
     </p>
-  </div >
+
+    <ul>
+      {repository.issues.edges.map(issue => (
+        <li key={issue.node.id}>
+          <a href={issue.node.url}>{issue.node.title}</a>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default App;
